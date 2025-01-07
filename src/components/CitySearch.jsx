df29dddaa1d2365e59683897672650c5725d9070
tpamplin <gartiman1@gmail.com>
2025-01-07 16:56:07 -0500

    move setup tests and pass CitySearch tests

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CitySearch = () => {
+    const [showSuggestions, setShowSuggestions] = useState(false);
     return (
         <div id='city-search'>
             <input 
                 type="text"
                 className="city"
                 placeholder="Search for a city"
+                onFocus={() => setShowSuggestions(true)}
             />
+            {showSuggestions? <ul className="suggestions"></ul> : null}
         </div>
     )
 }
