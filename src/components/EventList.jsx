1e604ffec7f0351c3c20d0c033b66bfa818b0240
tpamplin <gartiman1@gmail.com>
2025-01-07 15:35:53 -0500

    fix mapping key issue

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -5,7 +5,7 @@ import Event from "./Event"
 const EventList = ({ events }) => {
     return (
         <ul id='event-list'>
-            {events? events.map(event => <Event event={event} />): null }
+            {events? events.map(event => <Event key={event.id} event={event} />): null }
         </ul>
     );
 }
