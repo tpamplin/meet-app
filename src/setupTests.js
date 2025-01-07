a3774405126645e7b96149cd57fce7432ea3d572
tpamplin <gartiman1@gmail.com>
2025-01-07 17:00:56 -0500

    catch and fix errors

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,14 @@
 import "@testing-library/jest-dom";
+
+const MESSAGES_TO_IGNORE = [
+    "When testing, code that causes React state updates should be wrapped into act(...)",
+    "error",
+    "The above error occurred",
+];
+
+const originalError = console.error.bind(console.error);
+
+console.error = (...args) => {
+    const ignoreMessage = MESSAGES_TO_IGNORE.find((message) => args.toString().includes(message));
+    if (!ignoreMessage) originalError(...args);
+};
