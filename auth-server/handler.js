70939876ba35def8bdb776fbd30eedb2a9391491
tpamplin <gartiman1@gmail.com>
2025-01-07 09:46:36 -0500

    Build handler.js

---
+++ auth-server/handler.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const { google } = require("googleapis");
+const calendar = google.calendar("v3");
+const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
+const { CLIENT_ID, CLIENT_SECRET, CALENDAR_ID } = process.env;
+const redirect_uris = ["https://meet-app-jet.vercel.app/"];
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, redirect_uris[0]);
+
+module.exports.getAuthURL = async () => {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: "offline",
+        scope: SCOPES,
+    });
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify({
+            authUrl,
+        }),
+    };
+};
+
+module.exports.getAccessToken = async (event) => {
+    const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+    return new Promise((resolve, reject) => {
+        oAuth2Client.getToken(code, (error, response) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(response);
+        });
+    })
+        .then((results) => {
+            return {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Credentials": true,
+                },
+                body: JSON.stringify(results),
+            };
+        })
+        .catch((error) => {
+            return {
+                statusCode: 500,
+                body: JSON.stringify(error),
+            };
+        });
+};
