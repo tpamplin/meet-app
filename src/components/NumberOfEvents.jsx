79ece99af3e5b0b7ff0d43173097d4ca0a1ce622
tpamplin <gartiman1@gmail.com>
2025-01-08 17:23:57 -0500

    Update NumberOfEvents.jsx

--- src/components/NumberOfEvents.jsx
+++ src/components/NumberOfEvents.jsx
@@ -4,11 +4,19 @@ import React, {useState} from 'react';
 const NumberOfEvents = ({currentNOE, setCurrentNOE}) => {
 
     const [number, setNumber] = useState(currentNOE);
+
     const handleInputChange = (event) => {
-        const numberInput = event.target.value;
-        setCurrentNOE(numberInput);
-        setNumber(numberInput)
-    }
+        const value = event.target.value;
+        setNumber(value)
+        if(isNaN(value) || value <= 0) {
+            setErrorAlert('Enter a valid number');
+        } else if (value > 32) {
+            setErrorAlert('Only maximum of 32 is allowed');
+        } else {
+            setErrorAlert('');
+            setCurrentNOE(value);
+        }
+    };
 
     return (
         <div id='number-of-events'>
