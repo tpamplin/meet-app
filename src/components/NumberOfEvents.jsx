74682b831cf5092cb327e070e2560facc52ad778
tpamplin <gartiman1@gmail.com>
2025-01-08 17:12:22 -0500

    add local number state to stay in scope

--- src/components/NumberOfEvents.jsx
+++ src/components/NumberOfEvents.jsx
@@ -3,15 +3,17 @@ import React, {useState} from 'react';
 
 const NumberOfEvents = ({currentNOE, setCurrentNOE}) => {
 
+    const [number, setNumber] = useState(currentNOE);
     const handleInputChange = (event) => {
         const numberInput = event.target.value;
         setCurrentNOE(numberInput);
+        setNumber(numberInput)
     }
 
     return (
         <div id='number-of-events'>
             <label>Number of Events:</label>
-            <input type="text" value={currentNOE} onChange={handleInputChange} />
+            <input type="text" value={number} onChange={handleInputChange} />
         </div>
     );
 }
