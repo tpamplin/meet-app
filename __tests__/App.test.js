557147d0be0ea75deba8ee558a0f378201a24fdb
tpamplin <gartiman1@gmail.com>
2025-01-09 17:53:20 -0500

    write a new test to check if the number of events changes when updated by the user

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -26,11 +26,18 @@ describe("<App /> component", () => {
 });
 
 describe("<App /> integrations", () => {
-    test("renders a list of events matching the city selected by the user", async () => {
-        const user = userEvent.setup();
-        const AppComponent = render(<App />);
-        const AppDOM = AppComponent.container.firstChild;
+    let AppComponent;
+    let user;
+    let AppDOM;
+    let EventListDOM;
+    beforeEach(() => {
+        user = userEvent.setup();
+        AppComponent = render(<App />);
+        AppDOM = AppComponent.container.firstChild;
+        EventListDOM = AppDOM.querySelector("#event-list");
+    });
 
+    test("renders a list of events matching the city selected by the user", async () => {
         const CitySearchDOM = AppDOM.querySelector("#city-search");
         const CitySearchInput = within(CitySearchDOM).queryByRole("textbox");
 
@@ -38,16 +45,24 @@ describe("<App /> integrations", () => {
         const berlinSuggestionItem = within(CitySearchDOM).queryByText("Berlin, Germany");
         await user.click(berlinSuggestionItem);
 
-        const EventListDOM = AppDOM.querySelector("#event-list");
-        const allRenderedEventItems = within(EventListDOM).queryAllByRole("listitem");
-
         const allEvents = await getEvents();
         const berlinEvents = allEvents.filter((event) => event.location === "Berlin, Germany");
+
+        const allRenderedEventItems = within(EventListDOM).queryAllByRole("listitem");
 
         expect(allRenderedEventItems.length).toBe(berlinEvents.length);
 
         allRenderedEventItems.forEach((event) => {
             expect(event.textContent).toContain("Berlin, Germany");
         });
+    });
+
+    test("Renders the correct number of events when the user enters a new number", async () => {
+        const NumberOfEventsDOM = AppDOM.querySelector("#number-of-events");
+        const NumberOfEventsInput = within(NumberOfEventsDOM).queryByRole("textbox");
+        await user.type(NumberOfEventsInput, "{backspace}{backspace}15");
+        const allRenderedEventItems = within(EventListDOM).queryAllByRole("listitem");
+
+        expect(allRenderedEventItems.length).toBe(15);
     });
 });
