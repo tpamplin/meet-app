1322780d00ed2b13aa5e27ba1818b4d7b4d740f3
tpamplin <gartiman1@gmail.com>
2025-01-15 14:38:44 -0500

    organize

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as atatus from 'atatus-spa';
+
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration.js"
+
+import * as atatus from 'atatus-spa';
 atatus.config('13d0583355124d65a8aa028b252bb0e1').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
