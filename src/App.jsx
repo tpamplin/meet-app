9efd4fe8353b70656f545a817334112986e1edca
tpamplin <gartiman1@gmail.com>
2025-01-17 16:25:07 -0500

    create responsive styling for charts

--- src/App.jsx
+++ src/App.jsx
@@ -60,8 +60,8 @@ const App = () => {
         setErrorAlert={setErrorAlert}
       />
       <div className="charts-container">
-      <CityEventsChart allLocations={allLocations} events={events} /> 
-      <EventGenresChart events={events} />
+        <CityEventsChart allLocations={allLocations} events={events} />
+        <EventGenresChart events={events} />
       </div>
       <EventList events={events} />
 
