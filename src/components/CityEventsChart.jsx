1bf54e8b5b43ab986e91f4daba084605c7bf18a5
tpamplin <gartiman1@gmail.com>
2025-01-17 09:29:49 -0500

    add chart

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -9,8 +9,39 @@ import {
     ResponsiveContainer
 } from 'recharts';
 
-const CityEventsChart = () => {
+const CityEventsChart = ({ allLocations, events}) => {
+    const [data, setData] = useState([]);
+    
+    useEffect(() => {
+        setData(getData());
+    }, [`${events}`]);
 
+    const getData = () => {
+        const data = allLocations.map((location) => {
+            const count  = events.filter((event) => event.location === location).length;
+            console.log(count);
+            const city = location.split(', ')[0];
+            return {city, count};
+        })
+        return data;
+    }
+
+
+
+
+    return (
+        <ResponsiveContainer width="99%"  height={400}>
+            <ScatterChart margin={{top: 20, right: 20, left: 20, bottom: 20}}>
+                <CartesianGrid />
+                <XAxis type='category' dataKey='city' name='City'/>
+                <YAxis type='number' datakey='count' name='Number of events'/>
+                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                <Scatter name='Events' data={data} fill="#003300"/> 
+            </ScatterChart>
+        </ResponsiveContainer>
+
+
+    )
 }
 
 export default CityEventsChart;
